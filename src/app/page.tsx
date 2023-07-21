0db42fed43366a02899c26926ba6d0eba3b6ab0b
Mehmet <mehmetext68@gmail.com>
2023-07-21 22:02:36 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 import AddDeneme from "@/components/AddDeneme";
 import Denemes from "@/components/Denemes";
 import { getDenemes } from "@/lib/firebase";
+import { Metadata } from "next";
 
 export const revalidate = 0;
+
+export async function generateMetadata(): Promise<Metadata> {
+  const denemes: IDeneme[] = await getDenemes();
+
+  return {
+    title: "Total deneme: " + denemes.length,
+  };
+}
 
 export default async function Home() {
   const denemes: IDeneme[] = await getDenemes();
