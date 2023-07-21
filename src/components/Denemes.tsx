7f3759e4993290437ae2662dea29d5e82b957b40
Mehmet <mehmetext68@gmail.com>
2023-07-21 21:47:18 +0300

    Update Denemes.tsx

--- src/components/Denemes.tsx
+++ src/components/Denemes.tsx
@@ -1,22 +1,17 @@
 export default function Denemes({ denemes }: { denemes: IDeneme[] }) {
   return (
     <div className="flex flex-col gap-2">
-      {denemes.map((deneme) => (
+      {denemes.map((deneme, i) => (
         <div
           key={deneme.id}
-          className="flex justify-between gap-4 bg-blue-500 rounded p-2 text-white"
+          className={`flex justify-between gap-4 rounded-full pl-3 p-2 text-white ${
+            i % 2 == 0 ? "bg-blue-500" : "bg-blue-700"
+          }`}
         >
-          <p className="font-medium">
-            {deneme.text}{" "}
-            <span className="text-xs text-white/50">({deneme.id})</span>
-          </p>
-          <span>
+          <p className="font-medium">{deneme.text}</p>
+          <span className="shrink-0 bg-black/20 text-xs py-1 px-2 rounded-full">
             {deneme.createdAt?.toLocaleTimeString("tr-TR", {
               timeStyle: "short",
-            })}{" "}
-            -{" "}
-            {deneme.createdAt?.toLocaleDateString("tr-TR", {
-              dateStyle: "long",
             })}
           </span>
         </div>
